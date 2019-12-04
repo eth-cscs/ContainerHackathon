8f8fdd28f3929f540589804ac59ae69c2202a86f
jgp <jgp@cscs.ch>
2019-12-04 11:42:39 +0100

    perstep

--- NEMO/GYRE_namelist/filtering.sh
+++ NEMO/GYRE_namelist/filtering.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 in=$1
-elapsed=`grep 'timing step' $in |head -74 |tail -50 |awk '{s=s+$5}END{print s}'`
-echo $in $elapsed 50
+steps=50
+elapsed=`grep 'timing step' $in |head -74 |tail -$steps |awk '{s=s+$5}END{print s}'`
+time_per_step=`echo $elapsed $steps |awk '{print $1/$2}'`
+echo $in $elapsed $steps $time_per_step
